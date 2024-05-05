31f73355450631893b0a4e492a2e3e6fd166a38e
farukarslanCE <71271860+farukarslanCE@users.noreply.github.com>
2024-05-05 21:20:57 +0300

    Ahmet Faruk Arslan. Number of Rows & cleaning

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+data_set = pd.read_csv('2019-Oct.csv', nrows = 50)
+
+# , nrows = 50 kısmını silersen tüm data setle çalışıyosun, göstermesi kolay olsun diye duruyor
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
+
+data_set.dropna(subset=['category_code' , 'brand'], inplace=True)
+
+
+print("Number of rows in the dataset:", len(data_set))
+
